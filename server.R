ae5345d6ec6755c81b5effd56cdd952ed0fc64af
Rucknium <Rucknium@protonmail.com>
2021-08-02 23:17:26 -0400

    Performance boost: caching plot

--- server.R
+++ server.R
@@ -1,5 +1,9 @@
 
 
+#shinyOptions(cache = cachem::cache_disk(file.path(dirname(tempdir()), "myapp-cache")))
+shinyOptions(cache = cachem::cache_disk("./app_cache/cache/"))
+# see https://shiny.rstudio.com/articles/caching.html
+
 fusion.polished.data.dir <- "data/"
 # MUST have trailing /
 
@@ -33,52 +37,54 @@ server <- function(input, output, session) {
     paste0(prettyNum(fusions.summary.ls$n.bch, big.mark = ","),  " BCH")
   })
   
-  output$line_chart <- shiny::renderPlot({
-    fusions.date.agg.temp <- fusions.date.agg[
-      input$line_plot_date_range[1] <= as.Date(fusions.date.agg$Date) & 
-        as.Date(fusions.date.agg$Date) <= input$line_plot_date_range[2], ]
-    
-    par(mar = c(8, 4, 4, 2) + 0.1)
-    # c(bottom, left, top, right)
-
-    plot(fusions.date.agg.temp$Date, fusions.date.agg.temp$Freq, type = "n",
-      ylim = c(0, max(fusions.date.agg.temp$Freq)),
-      xaxt = "n",
-      main = "CashFusions per day",
-      ylab = "CashFusions per day", xlab = "")
-    
-    #axis.Date(1, format = "%Y-%m-%d")
-  
-   axis.POSIXct(1, at = seq(as.POSIXct( "2019-12-01", format = "%Y-%m-%d"), 
-     max(fusions.date.agg.temp$Date), by = "1 mon"), 
-     format = "%m/%Y", las = 2)
-   # Is hard-coded as.POSIXct( "2019-12-01", format = "%Y-%m-%d")
-   title(xlab = "Date", mgp = c(5.5, 1, 0))
-   # "Note that mgp[1] affects title whereas mgp[2:3] affect axis. The default is c(3, 1, 0))"
-
-    polygon(c(min(fusions.date.agg.temp$Date) - 1, fusions.date.agg.temp$Date, max(fusions.date.agg.temp$Date) + 1), 
-      c(-1, fusions.date.agg.temp$Freq, -1),
-      border = NA
-      , col = "#1b98e0" # , xpd = TRUE
+  output$line_chart <- shiny::bindCache( {
+    shiny::renderPlot({
+      fusions.date.agg.temp <- fusions.date.agg[
+        input$line_plot_date_range[1] <= as.Date(fusions.date.agg$Date) & 
+          as.Date(fusions.date.agg$Date) <= input$line_plot_date_range[2], ]
+      
+      par(mar = c(8, 4, 4, 2) + 0.1)
+      # c(bottom, left, top, right)
+      
+      plot(fusions.date.agg.temp$Date, fusions.date.agg.temp$Freq, type = "n",
+        ylim = c(0, max(fusions.date.agg.temp$Freq)),
+        xaxt = "n",
+        main = "CashFusions per day",
+        ylab = "CashFusions per day", xlab = "")
+      
+      #axis.Date(1, format = "%Y-%m-%d")
+      
+      axis.POSIXct(1, at = seq(as.POSIXct( "2019-12-01", format = "%Y-%m-%d"), 
+        max(fusions.date.agg.temp$Date), by = "1 mon"), 
+        format = "%m/%Y", las = 2)
+      # Is hard-coded as.POSIXct( "2019-12-01", format = "%Y-%m-%d")
+      title(xlab = "Date", mgp = c(5.5, 1, 0))
+      # "Note that mgp[1] affects title whereas mgp[2:3] affect axis. The default is c(3, 1, 0))"
+      
+      polygon(c(min(fusions.date.agg.temp$Date) - 1, fusions.date.agg.temp$Date, max(fusions.date.agg.temp$Date) + 1), 
+        c(-1, fusions.date.agg.temp$Freq, -1),
+        border = NA
+        , col = "#1b98e0" # , xpd = TRUE
       )
-    
-    lines(fusions.date.agg.temp$Date, fusions.date.agg.temp$moving.average.7.day, lwd = 2) # , col = "black"
-    legend("topleft", legend = "7-day moving average", lty = 1, lwd = 2)
-
-    if (input$fusion_friday) {
-      fusions.date.agg.temp.friday <- fusions.date.agg.temp[lubridate::wday(fusions.date.agg.temp$Date) == 6, ]
-      points(fusions.date.agg.temp.friday$Date,  fusions.date.agg.temp.friday$Freq, col = "red", cex = 1.5) #, pch = ".")
       
-      mtext(paste("CashFusions/day overall mean: ",
-        round(mean(fusions.date.agg.temp$Freq), digits = 1),
-        " | Friday mean: ", round(mean(fusions.date.agg.temp.friday$Freq, na.rm = TRUE), digits = 1)
+      lines(fusions.date.agg.temp$Date, fusions.date.agg.temp$moving.average.7.day, lwd = 2) # , col = "black"
+      legend("topleft", legend = "7-day moving average", lty = 1, lwd = 2)
+      
+      if (input$fusion_friday) {
+        fusions.date.agg.temp.friday <- fusions.date.agg.temp[lubridate::wday(fusions.date.agg.temp$Date) == 6, ]
+        points(fusions.date.agg.temp.friday$Date,  fusions.date.agg.temp.friday$Freq, col = "red", cex = 1.5) #, pch = ".")
+        
+        mtext(paste("CashFusions/day overall mean: ",
+          round(mean(fusions.date.agg.temp$Freq), digits = 1),
+          " | Friday mean: ", round(mean(fusions.date.agg.temp.friday$Freq, na.rm = TRUE), digits = 1)
         ), col = "red", line = 0.25)
+        
+      }
       
-    }
-    
-    par(mar = c(5, 4, 4, 2) + 0.1) #, mgp = c(3, 1, 0))
-    
-  })
+      par(mar = c(5, 4, 4, 2) + 0.1) #, mgp = c(3, 1, 0))
+      
+    })
+  }, input$line_plot_date_range, input$fusion_friday )
   
   output$fusion_txs_table <- DT::renderDataTable({
     fusions.df[, c("block.height", "block.time",  "txid.link", "value", 
