a8b2c4aed3216cd5e6a02aa2deb8e15221658ba3
Rucknium <Rucknium@protonmail.com>
2021-12-21 20:11:54 +0000

    Fix display issue with floating point precision

--- server.R
+++ server.R
@@ -151,8 +151,10 @@ server <- function(input, output, session) {
   })
   
   
-  output$fusion_txs_table <- DT::renderDataTable({
-    fusions.df()[, c("block.height", "block.time",  "txid.link", "value", 
+  output$fusion_txs_table <- DT::renderDataTable(
+    DT::formatRound(
+    DT::datatable({
+    fusions.df()[, c("block.height", "block.time",  "txid.link", "value", "tx.fee",
       "n.inputs", "n.outputs", "size", "txid", "block.time.orig",  "block.date" )]},
     # WARNING: If The above columns are changed, must also change "targets =" below
     rownames = FALSE,
@@ -160,8 +162,11 @@ server <- function(input, output, session) {
     escape = FALSE,
     style = "default",
     options = list(dom = "Blfrtip", buttons = I("colvis"), colReorder = list(realtime = FALSE),
-      columnDefs = list(list(targets = 8:10 - 1, visible = FALSE
-      ))) )
+      columnDefs = list(list(targets = 9:11 - 1, visible = FALSE
+      ))) ),
+    columns = c(4, 5), digits = 8) )
+  # Round the numbers: 
+  # https://stackoverflow.com/questions/44305021/r-shiny-renderdatatable-show-two-decimal-places-and-center-align-all-data
 
   # https://rstudio.github.io/DT/extensions.html
   # https://stackoverflow.com/questions/28117556/clickable-links-in-shiny-datatable
